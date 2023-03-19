c54870e32da23256e372d2accea3aa358a93e13b
William Mtemi Mponzi <willbrian009@gmail.com>
2023-03-19 15:13:47 +0300

    Completed

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+let num1= 2;
+let num2= 31;
 
-// Write your code here
+const multiply = num1*num2;
+
+const random =Math.floor(Math.random() * 10) + 1;
+
+let num3=14;
+let num4=5;
+
+const mod = num3%num4;
+
+const max = Math.max(1,5,9,17,20)
